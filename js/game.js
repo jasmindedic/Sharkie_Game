8e6a52aef856f03db19982d02b89a45a84b108a8
Jasmin Dedic <jdedic1999@gmail.com>
2022-11-24 09:53:26 +0100

    First commit

---
+++ js/game.js
@@ -0,0 +1,10 @@
+let canvas;
+
+let character = new MovableObject();
+
+function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    console.log("My character is", character);
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,11 @@
+class MovableObject {
+    // Attributes
+    x = 120;
+    y = 400;
+    image;
+
+    // Functions 
+    moveRight() {
+        console.log("Moving right");
+    }
+}
